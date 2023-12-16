68e46c1c6db50a49157e4a3ac942cd06cdd57072
emma2625 <emmanuelodobo10@gmail.com>
2023-12-16 15:52:23 +0100

    Update

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -27,7 +27,7 @@
                                 <td> {{ $item->room_name }} </td>
                                 <td> {{ $item->available_rooms }} </td>
                                 <td>₦ {{ number_format($item->room_price,2) }}</td>
-                                <td>14th Dec. 2023 9:00 am</td>
+                                <td> {{ $item->created_at->format('jS M. Y ') }} </td>
                                 <td>14th Dec. 2023 9:00 am</td>
                                 <td></td>
                             </tr>
