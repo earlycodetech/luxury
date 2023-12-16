6d08c9660443501faae7b545f5e900f5568426e6
emma2625 <emmanuelodobo10@gmail.com>
2023-12-16 15:57:32 +0100

    Update

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -5,7 +5,7 @@
             <div class="d-flex justify-content-between align-items-center mb-4">
                 <h1 class="text-center">All Rooms</h1>
 
-                <a href="" class="btn btn-primary">
+                <a href="{{ route('admin.room.create') }}" class="btn btn-primary">
                     New Room
                 </a>
             </div>
